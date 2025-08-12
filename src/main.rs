bf53c3c156994f79cc4a3844122bc038e896759c
harykali <harykali@outlook.com>
2025-08-12 22:18:20 +0800

    Hello world

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn great_world(){
+    let southern_germany = "Grüß Gott!";
+    let chinese="你好，世界";
+    let english="world hello";
+    let regions = [southern_germany,chinese,english];
+    for region in regions.iter(){
+        println!("{}",&region);
+    }
+
+
+}
+
+fn main() {
+    great_world();
+}
