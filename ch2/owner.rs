06f5f2f49cc01c8439b080052d60d587bb079e66
harykali <harykali@outlook.com>
2025-08-24 20:49:50 +0800

    daily update

--- ch2/owner.rs
+++ ch2/owner.rs
@@ -43,8 +43,57 @@
 // fn print_str(s1: &str) { // Accept a string slice (&str) instead of String
 //     println!("{}", s1); // Directly use the reference
 // }
+// fn main() {
+//     let x = (1, 2, (), "hello".to_string());
+//     let y = &x;
+//     println!("{:?}, {:?}", x, y);
+// }
+
+
+// fn main() {
+//     let s = String::from("hello, ");
+//
+//     // 只修改下面这行代码 !
+//     let mut s1 = s;
+//
+//     s1.push_str("world");
+//     println!("{}",s1);
+// }
+
+
+// fn main() {
+//     let x = Box::new(5);
+//
+//     let mut y=Box::new(4);      // 完成该行代码，不要修改其它行！
+//
+//     *y = 4;
+//
+//     assert_eq!(*x, 5);
+// }
+
+// fn main() {
+//     let t = (String::from("hello"), String::from("world"));
+//
+//     let _s = t.0;
+//
+//     // 仅修改下面这行代码，且不要使用 `_s`
+//     println!("{:?}", t.1);
+// }
+
+// fn main() {
+//     let t = (String::from("hello"), String::from("world"));
+//
+//     // 填空，不要修改其它代码
+//     let (__, __) = __;
+//
+//     println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+// }
+
 fn main() {
-    let x = (1, 2, (), "hello".to_string());
-    let y = &x;
-    println!("{:?}, {:?}", x, y);
+    let t = (String::from("hello"), String::from("world"));
+
+    // 填空，不要修改其它代码
+    let (s1, s2) = (&t.0,&t.1);
+
+    println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
 }
