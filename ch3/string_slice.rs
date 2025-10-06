858a97eef961ea6d4b05a1ad29cffb013ec29489
HaryKali K <harykali@outlook.com>
2025-10-06 21:46:19 +0800

    update

--- ch3/string_slice.rs
+++ ch3/string_slice.rs
@@ -232,20 +232,97 @@
 //     let s1: &str = s;
 // }
 /* 填空并修复所有错误 */
+// fn main() {
+//     let raw_str = "Escapes don't work here: \x3F \u{211D}";
+//     // 修改上面的行让代码工作
+//     assert_eq!(raw_str, "Escapes don't work here: ? ℝ");
+//
+//     // 如果你希望在字符串中使用双引号，可以使用以下形式
+//     let quotes = r#"And then I said: "There is no escape!""#;
+//     println!("{}", quotes);
+//
+//     // 如果希望在字符串中使用 # 号，可以如下使用：
+//     let  delimiter = r###"A string with "# in it. And even "##!"###;
+//     println!("{}", delimiter);
+//
+//     // 填空
+//     let long_delimiter = r###"Hello, "##""###;
+//     assert_eq!(long_delimiter, "Hello, \"##\"")
+// }
+
+//slice
+
+
+// 修复代码中的错误，不要新增代码行!
+// fn main() {
+//     let arr = [1, 2, 3];
+//     let s1: &[i32] = &arr[0..2];
+//
+//     let s2: &str = "hello, world" as &str;
+// }
+
+
+// fn main() {
+//     let arr: [char; 3] = ['中', '国', '人'];
+//     let slice_pre = String::from("Hello world");
+//     let slice = &slice_pre[..3];
+//
+//     // 修改数字 `8` 让代码工作
+//     // 小提示: 切片和数组不一样，它是引用。如果是数组的话，那下面的 `assert!` 将会通过： '中'和'国'是char类型，char类型是Unicode编码，大小固定为4字节，两个字符为8字节。
+//     assert!(std::mem::size_of_val(&slice) == 18);
+// }
+
+
+// fn main() {
+//     let arr: [char; 3] = ['中', '国', '人'];
+//
+//     let slice = &arr[..2];
+//
+//     // 修改数字 `8` 让代码工作
+//     // 小提示: 切片和数组不一样，它是引用。如果是数组的话，那下面的 `assert!` 将会通过： '中'和'国'是char类型，char类型是Unicode编码，大小固定为4字节，两个字符为8字节。
+//     assert!(std::mem::size_of_val(&slice) == 8);
+// }
+
+
+// fn main() {
+//     let arr: [i32; 5] = [1, 2, 3, 4, 5];
+//     // 填空让代码工作起来
+//     let slice:&[i32]  =&arr[1..4];
+//     assert_eq!(slice, &[2, 3, 4]);
+// }
+
+
+// fn main() {
+//     let s = String::from("hello");
+//
+//     let slice1 = &s[0..2];
+//     // 填空，不要再使用 0..2
+//     let slice2 = &s[..2];
+//
+//     assert_eq!(slice1, slice2);
+// }
+
+// fn main() {
+//     let s = "你好，世界";
+//     // 修改以下代码行，让代码工作起来
+//     let slice = &s[0..3];
+//
+//     assert!(slice == "你");
+// }
+
+
+// 修复所有错误
 fn main() {
-    let raw_str = "Escapes don't work here: \x3F \u{211D}";
-    // 修改上面的行让代码工作
-    assert_eq!(raw_str, "Escapes don't work here: ? ℝ");
+    let mut s = String::from("hello world");
 
-    // 如果你希望在字符串中使用双引号，可以使用以下形式
-    let quotes = r#"And then I said: "There is no escape!""#;
-    println!("{}", quotes);
+    // 这里, &s 是 `&String` 类型，但是 `first_character` 函数需要的是 `&str` 类型。
+    // 尽管两个类型不一样，但是代码仍然可以工作，原因是 `&String` 会被隐式地转换成 `&str` 类型，如果大家想要知道更多，可以看看 Deref 章节: https://course.rs/advance/smart-pointer/deref.html
+    let ch = first_character(&s);
 
-    // 如果希望在字符串中使用 # 号，可以如下使用：
-    let  delimiter = r###"A string with "# in it. And even "##!"###;
-    println!("{}", delimiter);
+    s.to_string().clear(); // error!
 
-    // 填空
-    let long_delimiter = r###"Hello, "##""###;
-    assert_eq!(long_delimiter, "Hello, \"##\"")
+    println!("the first character is: {}", ch);
+}
+fn first_character(s: &str) -> &str {
+    &s[..1]
 }
