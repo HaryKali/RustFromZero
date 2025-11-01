0cd9b3f706463ee0c72b81db75324d2a46b3546e
HaryKali K <harykali@outlook.com>
2025-11-01 12:13:25 +0800

    lazy update

--- ch5/pattern_match.rs
+++ ch5/pattern_match.rs
@@ -67,43 +67,139 @@
 //
 //     println!("{}", ip_str);
 // }
-#[derive(Debug)]
-
-enum UsState {
-    Alabama,
-    Alaska,
-}
 
 
+// enum UsState {
+//     Alabama,
+//     Alaska,
+// }
+//
+//
+//
+// enum Coin{
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+//
+//
+// }
+//
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => {
+//             println!("Lucky!!!");
+//             1
+//         }
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(UsState) =>{
+//             println!("State quarter from {:?}!", UsState);
+//             25
+//         }
+//
+//     }
+// }
+// fn main() {
+//
+//     let test_coin=Coin::Quarter(UsState::Alaska);
+//     println!("{}", value_in_cents(test_coin));
+//
+//
 
-enum Coin{
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
+// enum Action{
+//     Say(String),
+//     MoveTo(i32,i32),
+//     ChangeColor(u16,u16,u16)
+//
+//
+//
+//
+//
+// }
+//
+// fn main(){
+//     let actions = [Action::Say("Hello, World!".to_string()),
+//         Action::MoveTo(10,20),
+//         Action::ChangeColor(5,6,7),];
+//
+//     for action in &actions {
+//         match action {
+//             Action::Say(s) => {println!("{}",s); },
+//             Action::MoveTo(x,y) => {println!("{:?}",(x,y));},
+//             Action::ChangeColor(r,g,b) => {println!("{:?}",(r,g,b));},
+//
+//
+//         }
+//
+//
+//
+//     }
+//
+//
+//
+// }
+// enum Direction {
+//     East,
+//     West,
+//     North,
+//     South,
+// }
+//
+// fn main() {
+//     let dire = Direction::South;
+//     match dire {
+//         Direction::East => println!("East"),
+//         Direction::North | Direction::South => {
+//             println!("South or North");
+//         },
+//     };
+// }
 
+// fn main(){
+//
+//     let some_u8_value = 0u8;
+//     match some_u8_value {
+//         1 => println!("one"),
+//         3 => println!("three"),
+//         5 => println!("five"),
+//         7 => println!("seven"),
+//         _ => (),
+//     }
+//
+//
+// }
 
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky!!!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(UsState) =>{
-            println!("State quarter from {:?}!", UsState);
-            25
-        }
-
-    }
-}
-fn main() {
-
-    let test_coin=Coin::Quarter(UsState::Alaska);
-    println!("{}", value_in_cents(test_coin));
-
-
-}
+// #[derive(Debug)]
+// enum Direction {
+//     East,
+//     West,
+//     North,
+//     South,
+// }
+//
+// fn main() {
+//     let dire = Direction::South;
+//     match dire {
+//         Direction::East => println!("East"),
+//         orther => println!("other direction: {:?}", other),
+//     };
+// }
+// fn main() {
+//     let age = Some(30);
+//     println!("在匹配前，age是{:?}", age);
+//     match age {
+//         Some(x) =>  println!("匹配出来的age是{}", x),
+//         _ => ()
+//     }
+//     println!("在匹配后，age是{:?}", age);
+// }
+// fn main() {
+//     let age = Some(30);
+//     println!("在匹配前，age是{:?}",age);
+//     if let Some(age) = age {
+//         println!("匹配出来的age是{}",age);
+//     }
+//
+//     println!("在匹配后，age是{:?}",age);
+// }
