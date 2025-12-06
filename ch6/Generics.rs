fc5d0dc5e97a4f8e967c73bc28ece7d09d488f56
HaryKali K <harykali@outlook.com>
2025-12-06 12:07:51 +0800

    Lazy update

--- ch6/Generics.rs
+++ ch6/Generics.rs
@@ -237,3 +237,85 @@
 //     display_array(arr);
 // }
 //111
+
+
+// 填空
+// struct A;          // 具体的类型 `A`.
+// struct S(A);       // 具体的类型 `S`.
+// struct SGen<T>(T); // 泛型 `SGen`.
+//
+// fn reg_fn(_s: S) {}
+//
+// fn gen_spec_t(_s: SGen<A>) {}
+//
+// fn gen_spec_i32(_s: SGen<i32>) {}
+//
+// fn generic<T>(_s: SGen<T>) {}
+//
+// fn main() {
+//     // 使用非泛型函数
+//     reg_fn(S("123"));          // 具体的类型
+//     gen_spec_t(SGen('A'));   // 隐式地指定类型参数  `A`.
+//     gen_spec_i32(SGen(123)); // 隐式地指定类型参数`i32`.
+//
+//     // 显式地指定类型参数 `char`
+//     generic::<char>(SGen('a'));
+//
+//     // 隐式地指定类型参数 `char`.
+//     generic(SGen(2));
+// }
+
+
+// 实现下面的泛型函数 sum
+// fn sum<T: std::ops::Add<Output=T>>(num1: T, num2: T) -> T{
+//     num1 + num2
+// }
+//
+// fn main() {
+//     assert_eq!(5, sum(2i8, 3i8));
+//     assert_eq!(50, sum(20, 30));
+//     assert_eq!(2.46, sum(1.23, 1.23));
+// }
+
+
+// 实现一个结构体 Point 让代码工作
+
+// struct Point<T,U> {
+//     x:T,
+//     y:U
+// }
+//
+// fn main() {
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+// }
+
+// 修改以下结构体让代码工作
+// struct Point<T,U> {
+//     x: T,
+//     y: U,
+// }
+//
+// fn main() {
+//     // 不要修改这行代码！
+//     let p = Point{x: 5, y : "hello".to_string()};
+// }
+
+
+// 为 Val 增加泛型参数，不要修改 `main` 中的代码
+// struct Val<T> {
+//     val: T,
+// }
+//
+// impl<T> Val<T> {
+//     fn value(&self) -> &T {
+//         &self.val
+//     }
+// }
+//
+//
+// fn main() {
+//     let x = Val{ val: 3.0 };
+//     let y = Val{ val: "hello".to_string()};
+//     println!("{}, {}", x.value(), y.value());
+// }
