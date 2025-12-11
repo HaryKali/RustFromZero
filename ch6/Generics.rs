98d61054f6a53690f3fcdab0bb21d56f14ab340e
HaryKali K <harykali@outlook.com>
2025-12-11 16:22:13 +0800

    lazy update

--- ch6/Generics.rs
+++ ch6/Generics.rs
@@ -350,29 +350,41 @@
 //     let arr = ["hello", "world"];
 //     print_array(arr);
 // }
-#![allow(incomplete_features)]
-#![feature(generic_const_exprs)]
+// #![allow(incomplete_features)]
+// #![feature(generic_const_exprs)]
+//
+// fn check_size<T>(val: T)
+// where
+//     Assert<{ core::mem::size_of::<T>() < 768 }>: IsTrue,
+// {
+//     //...
+// }
+//
+// // fix the errors in main
+// fn main() {
+//     check_size([0u8; 767]);
+//     check_size([0i32; 191]);
+//     check_size(["hello你好"; 47]);
+//     check_size([(); 31].map(|_| "hello你好".to_string()));
+//     check_size(['中'; 191]);
+// }
+//
+//
+//
+// pub enum Assert<const CHECK: bool> {}
+//
+// pub trait IsTrue {}
+//
+// impl IsTrue for Assert<true> {}
 
-fn check_size<T>(val: T)
-where
-    Assert<{ core::mem::size_of::<T>() < 768 }>: IsTrue,
-{
-    //...
-}
-
-// fix the errors in main
 fn main() {
-    check_size([0u8; 767]);
-    check_size([0i32; 191]);
-    check_size(["hello你好"; 47]);
-    check_size([(); 31].map(|_| "hello你好".to_string()));
-    check_size(['中'; 191]);
+    let mut base=(0,1);
+    let mut fib= ||{
+        base=(base.1,base.0+base.1);
+        base.0
+    };
+    for _ in 1..20 {
+        println!("{}", fib());
+    }
+
 }
-
-
-
-pub enum Assert<const CHECK: bool> {}
-
-pub trait IsTrue {}
-
-impl IsTrue for Assert<true> {}
