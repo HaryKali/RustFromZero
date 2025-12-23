4cf06fc6be6678be13c8d6b160f03080fbf41d46
HaryKali K <harykali@outlook.com>
2025-12-23 22:32:09 +0800

    lazy update

--- ch6/Trait.rs
+++ ch6/Trait.rs
@@ -38,33 +38,84 @@ pub struct Weibo{
 //     println!("Post username: {}", post.summarize());
 // }
 
-pub trait Summary {
-    fn summarize_author(&self) -> String;
+// pub trait Summary {
+//     fn summarize_author(&self) -> String;
+//
+//     fn summarize(&self) -> String {
+//         format!("(Read more from {}...)", self.summarize_author())
+//     }
+// }
+//
+//
+// // impl Summary for Post{}
+// impl Summary for Weibo{
+//     fn summarize_author(&self) -> String{
+//         format!("@{}", self.username)
+//     }
+// }
+// impl Summary for Post {
+//     fn summarize(&self) -> String {
+//         format!("{} by {}", self.title, self.author)
+//     }
+//
+//     fn summarize_author(&self) -> String{
+//         format!("@{}", self.author)
+//     }
+// }
+//
+// pub fn notify(item: &impl Summary) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+//
+// pub fn notify2<T: Summary>(item: &T){
+//
+//     println!("Breaking news! {}", item.summarize());
+// }
+//
+// pub fn notify3<T: Summary>(item1: &T, item2: &T){
+//     println!("Item1：Breaking news! {}", item1.summarize());
+//     println!("Item2: Breaking news! {}", item2.summarize());
+// }
+// pub fn notify4(item1: &impl Summary, item2: &impl Summary){
+//     println!("Item1：Breaking news! {}", item1.summarize());
+//     println!("Item2: Breaking news! {}", item2.summarize());
+// }
+//
+//
+// fn main() {
+//     let post= Post{title: String::from("Test"), author: String::from("Michael"), content: String::from("Hello World!") };
+//     let weibo = Weibo{content: String::from("Hello World!"), username: String::from("Michael") };
+//     // println!("{}", post.summarize());
+//     // println!("{}", weibo.summarize());
+//     // notify(&weibo);
+//     // notify2(&weibo);
+//     // notify3(&weibo,&post);
+//     notify4(&weibo,&post);
+//
+// }
+use std::fmt::Display;
 
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self {
+            x,
+            y,
+        }
     }
 }
 
-
-// impl Summary for Post{}
-impl Summary for Weibo{
-    fn summarize_author(&self) -> String{
-        format!("@{}", self.username)
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
     }
-}
-
-pub fn notify(item: &impl Summary) {
-    println!("Breaking news! {}", item.summarize());
-}
-
-
-fn main() {
-    // let post= Post{title: String::from("Test"), author: String::from("Michael"), content: String::from("Hello World!") };
-    let weibo = Weibo{content: String::from("Hello World!"), username: String::from("Michael") };
-    // println!("{}", post.summarize());
-    // println!("{}", weibo.summarize());
-    notify(&weibo);
-
 }
 
