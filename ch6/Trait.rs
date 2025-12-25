65c52d476bbb3ec897460786c26c0e6f2cbd97de
HaryKali K <harykali@outlook.com>
2025-12-25 11:20:43 +0800

    not that lazy update

--- ch6/Trait.rs
+++ ch6/Trait.rs
@@ -1,16 +1,16 @@
 // pub trait Summary {
 //     fn summarize(&self) -> String;
 // }
-pub struct Post {
-    pub title: String,
-    pub author: String,
-    pub content: String,
-}
-
-pub struct Weibo{
-    pub content: String,
-    pub username: String,
-}
+// pub struct Post {
+//     pub title: String,
+//     pub author: String,
+//     pub content: String,
+// }
+//
+// pub struct Weibo{
+//     pub content: String,
+//     pub username: String,
+// }
 //
 // impl Post{
 //     fn init(){
@@ -93,29 +93,89 @@ pub struct Weibo{
 //     notify4(&weibo,&post);
 //
 // }
-use std::fmt::Display;
+// use std::fmt::Display;
+//
+// struct Pair<T,Z> {
+//     x: T,
+//     y: Z,
+// }
+//
+// impl<T,Z> Pair<T,Z> {
+//     fn new(x: T, y: Z) -> Self {
+//         Self {
+//             x,
+//             y,
+//         }
+//     }
+// }
+//
+// impl<T: Display + PartialOrd,Z> Pair<T,Z> {
+//     fn cmp_display(&self) {
+//         if self.x >= self.y {
+//             println!("The largest member is x = {}", self.x);
+//         } else {
+//             println!("The largest member is y = {}", self.y);
+//         }
+//     }
+// }
 
-struct Pair<T> {
-    x: T,
-    y: T,
-}
+// fn main(){
+//     let test_pair=Pair{x: 'c', y: 3.11};
+//     test_pair.cmp_display();
+//
+// }
+// fn largest<T:PartialOrd+ Copy>(list: &[T]) -> T {
+//     let mut largest = list[0];
+//
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//
+//     largest
+// }
+//
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+//
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+//
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+//
+//     let result = largest(&char_list);
+//     println!("The largest char is {}", result);
+// }
 
-impl<T> Pair<T> {
-    fn new(x: T, y: T) -> Self {
-        Self {
-            x,
-            y,
-        }
-    }
-}
-
-impl<T: Display + PartialOrd> Pair<T> {
-    fn cmp_display(&self) {
-        if self.x >= self.y {
-            println!("The largest member is x = {}", self.x);
-        } else {
-            println!("The largest member is y = {}", self.y);
-        }
-    }
-}
-
+// use std::ops::Add;
+//
+// #[derive(Debug)]
+//
+// struct Point<T: Add<T,Output = T>>{
+//     x:T,
+//     y:T,
+//
+// }
+//
+// impl <T: Add<T,Output = T>> Add for Point<T> {
+//     type Output = Point<T>;
+//     fn add(self, p:Point<T>) -> Point<T> {
+//         Point {
+//             x: self.x + p.x,
+//             y: self.y + p.y,
+//         }
+//     }
+// }
+// fn add<T: Add<T,Output = T>>(x:T,y:T) -> T {
+//     x+y
+// }
+// fn main() {
+//     let p1 = Point{x: 1.1f32, y: 1.1f32};
+//     let p2 = Point{x: 2.1f32, y: 2.1f32};
+//     println!("{:?}", add(p1, p2));
+//
+//     let p3 = Point{x: 1i32, y: 1i32};
+//     let p4 = Point{x: 2i32, y: 2i32};
+//     println!("{:?}", p3+p4);
+// }
