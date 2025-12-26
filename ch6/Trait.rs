a7010197b468688a09cccbc1f2abe0a10f8de2de
HaryKali K <harykali@outlook.com>
2025-12-26 22:53:49 +0800

    Not that lazy update

--- ch6/Trait.rs
+++ ch6/Trait.rs
@@ -179,3 +179,106 @@
 //     let p4 = Point{x: 2i32, y: 2i32};
 //     println!("{:?}", p3+p4);
 // }
+
+// 完成两个 `impl` 语句块
+// 不要修改 `main` 中的代码
+// trait Hello {
+//     fn say_hi(&self) -> String {
+//         String::from("hi")
+//     }
+//
+//     fn say_something(&self) -> String;
+// }
+//
+// struct Student {}
+// impl Hello for Student {
+//     fn say_hi(&self) -> String {
+//         String::from("hi")
+//     }
+//     fn say_something(&self) -> String {
+//         String::from("I'm a good student")
+//     }
+// }
+// struct Teacher {}
+// impl Hello for Teacher {
+//     fn say_hi(&self) -> String {
+//         String::from("Hi, I'm your new teacher")
+//     }
+//     fn say_something(&self) -> String {
+//         String::from("I'm not a bad teacher")
+//     }
+// }
+//
+// fn main() {
+//     let s = Student {};
+//     assert_eq!(s.say_hi(), "hi".to_string());
+//     assert_eq!(s.say_something(), "I'm a good student".to_string());
+//
+//     let t = Teacher {};
+//     assert_eq!(t.say_hi(), "Hi, I'm your new teacher".to_string());
+//     assert_eq!(t.say_something(), "I'm not a bad teacher".to_string());
+//
+//     println!("Success!")
+// }
+
+
+
+// `Centimeters`, 一个元组结构体，可以被比较大小
+// #[derive(PartialEq, PartialOrd)]
+// struct Centimeters(f64);
+//
+// // `Inches`, 一个元组结构体可以被打印
+// #[derive(Debug)]
+// #[derive(PartialEq, PartialOrd)]
+// struct Inches(i32);
+//
+// impl Inches {
+//     fn to_centimeters(&self) -> Centimeters {
+//         let &Inches(inches) = self;
+//
+//         Centimeters(inches as f64 * 2.54)
+//     }
+// }
+//
+//
+// #[derive(PartialEq, PartialOrd)]
+// #[derive(Debug)]
+// struct Seconds(i32);
+//
+// fn main() {
+//     let _one_second = Seconds(1);
+//
+//     println!("One second looks like: {:?}", _one_second);
+//     let _this_is_true = _one_second == _one_second;
+//     let _this_is_false = _one_second > _one_second;
+//
+//     let foot = Inches(12);
+//
+//     println!("One foot equals {:?}", foot);
+//
+//     let meter = Centimeters(100.0);
+//
+//     let cmp =
+//         if foot.to_centimeters() < meter {
+//             "smaller"
+//         } else {
+//             "bigger"
+//         };
+//
+//     println!("One foot is {} than one meter.", cmp);
+// }
+
+
+use std::ops;
+
+// implement fn multiply to make the code work
+// As mentioned above, `+` needs `T` to implement `std::ops::Add` Trait
+// so, what about `*` ?  You can find the answer here: https://doc.rust-lang.org/core/ops/
+fn multiply<T: ops::Mul<Output = T>>(x: T, y: T) -> T {
+    x * y
+}
+
+fn main() {
+    assert_eq!(6, multiply(2u8, 3u8));
+    assert_eq!(5.0, multiply(1.0, 5.0));
+}
