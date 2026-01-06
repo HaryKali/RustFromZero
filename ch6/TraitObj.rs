61c695f4504ceae9ca8d726284a6c8f9e82af019
HaryKali <HaryKali@outlook.com>
2026-01-06 20:04:04 +0800

    lazy update

--- ch6/TraitObj.rs
+++ ch6/TraitObj.rs
@@ -205,3 +205,103 @@
 //
 //     }
 // }
+
+
+
+// 填空
+// trait Draw {
+//     fn draw(&self) -> String;
+// }
+//
+// impl Draw for u8 {
+//     fn draw(&self) -> String {
+//         format!("u8: {}", *self)
+//     }
+// }
+//
+// impl Draw for f64 {
+//     fn draw(&self) -> String {
+//         format!("f64: {}", *self)
+//     }
+// }
+//
+// fn main() {
+//     let x = 1.1f64;
+//     let y = 8u8;
+//
+//     // draw x
+//     draw_with_box(Box::new(x));
+//
+//     // draw y
+//     draw_with_ref(&y);
+//
+//     println!("Success!")
+// }
+//
+// fn draw_with_box(x: Box<dyn Draw>) {
+//     x.draw();
+// }
+//
+// fn draw_with_ref(x: &u8) {
+//     x.draw();
+// }
+
+
+
+// trait Foo {
+//     fn method(&self) -> String;
+// }
+//
+// impl Foo for u8 {
+//     fn method(&self) -> String { format!("u8: {}", *self) }
+// }
+//
+// impl Foo for String {
+//     fn method(&self) -> String { format!("string: {}", *self) }
+// }
+//
+// // 通过泛型实现以下函数
+// fn static_dispatch<T:Foo>(x:T){
+//     x.method();
+//
+// }
+//
+// // 通过特征对象实现以下函数
+// fn dynamic_dispatch(x: &dyn Foo){
+//     x.method();
+//
+// }
+//
+// fn main() {
+//     let x = 5u8;
+//     let y = "Hello".to_string();
+//
+//     static_dispatch(x);
+//     dynamic_dispatch(&y);
+//
+//     println!("Success!")
+// }
+
+
+// 使用至少两种方法让代码工作
+// 不要添加/删除任何代码行
+trait MyTrait {
+    fn f(&self) -> Box<dyn MyTrait>;
+}
+
+impl MyTrait for u32 {
+    fn f(&self) -> Box<dyn MyTrait> { Box::new(42) }
+}
+
+impl MyTrait for String {
+    fn f(&self) -> Box<dyn MyTrait> { Box::new(self.clone()) }
+}
+
+fn my_function(x: Box<dyn MyTrait>) -> Box<dyn MyTrait> {
+    x.f()
+}
+
+fn main() {
+    my_function(Box::new(13_u32));
+    my_function(Box::new(String::from("abc")));
+}
