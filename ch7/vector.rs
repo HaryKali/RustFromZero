daeebe9d778360601c3156c30662889498a7300c
HaryKali <HaryKali@outlook.com>
2026-01-21 12:09:28 +0800

    lazy update

--- ch7/vector.rs
+++ ch7/vector.rs
@@ -38,30 +38,110 @@
 //
 // }
 
-trait IpAddr {
-    fn display(&self);
-}
+// trait IpAddr {
+//     fn display(&self);
+// }
+//
+// struct V4(String);
+// impl IpAddr for V4 {
+//     fn display(&self) {
+//         println!("ipv4: {:?}",self.0)
+//     }
+// }
+// struct V6(String);
+// impl IpAddr for V6 {
+//     fn display(&self) {
+//         println!("ipv6: {:?}",self.0)
+//     }
+// }
+//
+// fn main() {
+//     let v: Vec<Box<dyn IpAddr>> = vec![
+//         Box::new(V4("127.0.0.1".to_string())),
+//         Box::new(V6("::1".to_string())),
+//     ];
+//
+//     for ip in v {
+//         ip.display();
+//     }
+// }
 
-struct V4(String);
-impl IpAddr for V4 {
-    fn display(&self) {
-        println!("ipv4: {:?}",self.0)
-    }
-}
-struct V6(String);
-impl IpAddr for V6 {
-    fn display(&self) {
-        println!("ipv6: {:?}",self.0)
-    }
-}
+// fn main() {
+//     let mut vec = vec![1, 5, 10, 2, 15];
+//     vec.sort_unstable();
+//     assert_eq!(vec, vec![1, 2, 5, 10, 15]);
+//     for num in vec {
+//         println!("{}", num);
+//     }
+//
+// }
 
-fn main() {
-    let v: Vec<Box<dyn IpAddr>> = vec![
-        Box::new(V4("127.0.0.1".to_string())),
-        Box::new(V6("::1".to_string())),
-    ];
+// #[derive(Debug)]
+// struct Person {
+//     name: String,
+//     age: u32,
+// }
+//
+// impl Person {
+//     fn new(name: String, age: u32) -> Person {
+//         Person { name, age }
+//     }
+// }
+//
+// fn main() {
+//     let mut people = vec![
+//         Person::new("Zoe".to_string(), 25),
+//         Person::new("Al".to_string(), 60),
+//         Person::new("John".to_string(), 1),
+//     ];
+//     people.sort_unstable_by(|a, b| b.age.cmp(&a.age));
+//
+//     println!("{:?}", people);
+// }
 
-    for ip in v {
-        ip.display();
-    }
-}
+
+// fn main() {
+//     let arr: [u8; 3] = [1, 2, 3];
+//
+//     let v = Vec::from(arr);
+//     is_vec(&v);
+//
+//     let v = vec![1, 2, 3];
+//     is_vec(&v);
+//
+//     // vec!(..) 和 vec![..] 是同样的宏，宏可以使用 []、()、{}三种形式，因此...
+//     let v = vec!(1, 2, 3);
+//     is_vec(&v);
+//
+//     // ...在下面的代码中, v 是 Vec<[u8; 3]> , 而不是 Vec<u8>
+//     // 使用 Vec::new 和 `for` 来重写下面这段代码
+//     let mut v1=Vec::new();
+//
+//     for num in arr{
+//         v1.push(num);
+//     }
+//     let v2=&v1;
+//
+//     is_vec(&v1);
+//
+//     assert_eq!(v, v1);
+//
+//     println!("Success!")
+// }
+//
+// fn is_vec(v: &Vec<u8>) {}
+
+
+// 填空
+// fn main() {
+//     let mut v1 = Vec::from([1, 2, 4]);
+//     v1.pop();
+//     v1.push(3);
+//
+//     let mut v2 = Vec::new();
+//     v2.extend(vec![1, 2, 3]);
+//
+//     assert_eq!(v1, v2);
+//
+//     println!("Success!")
+// }
